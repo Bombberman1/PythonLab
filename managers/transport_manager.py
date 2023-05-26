3a2704bfddcc59e19de39fd9bf32ad08ed566546
Oleg <lezik569@gmail.com>
2023-05-26 19:17:53 +0300

    Adding lab 9 files

--- managers/transport_manager.py
+++ managers/transport_manager.py
@@ -1,24 +1,93 @@
 """manager"""
+import time
+
 from models.trolleybus import Trolleybus
 
-transports = []
+
+def executiontime(func):
+    """print function's execution time"""
+    def timer(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"Execution time: {end - start} seconds")
+        return result
+
+    return timer
 
 
-def add_transport(transport):
-    """add transport to list"""
-    transports.append(transport)
+def converter(func):
+    """convert type, which is gotten from method, to tuple()"""
+    def change_iterator(*args, **kwargs):
+        func_res = func(*args, **kwargs)
+        if isinstance(func_res, dict) and len(func_res) > 0:
+            return tuple(func_res.items())
+        return tuple(func(*args, **kwargs))
+
+    return change_iterator
 
 
-def find_all_with_id_greater_than(number):
-    """get list <Transport> when id > number"""
-    return [obj for obj in transports if obj.identifier > number]
+class TransportManager:
+    """class contains list of Transport objects"""
+    transports = []
 
+    def __len__(self):
+        return len(self.transports)
 
-def find_all_with_current_speed():
-    """get list <Transport> when current_speed > number"""
-    return [obj for obj in transports if obj.current_speed > 0]
+    def __getitem__(self, item):
+        return self.transports[item]
 
+    def __iter__(self):
+        return iter(self.transports)
 
-def find_all_trolleybuses_with_passengers(number):
-    """get list <Trolleybus> when passengers == number"""
-    return [obj for obj in transports if isinstance(obj, Trolleybus) if obj.passengers is number]
+    @executiontime
+    @converter
+    def get_results(self):
+        """return list of results of accelerate() method"""
+        for temp in self.transports:
+            temp.accelerate(30)
+        return [result.current_speed for result in self.transports]
+
+    @executiontime
+    def get_enumerate_list_table(self):
+        """return tuple of Transport objects with numeration"""
+        return [(pos, type(transport).__name__ + ": " + str(transport))
+                for pos, transport in enumerate(self.transports)]
+
+    @executiontime
+    def get_zip_list_table(self):
+        """return tuple of Transport objects with accelerate() results"""
+        for temp in self.transports:
+            temp.accelerate(60)
+        return [(type(transport).__name__ + ": " + str(transport), speed)
+                for transport, speed in
+                zip(self.transports,
+                    [transport.current_speed for transport in self.transports])]
+
+    @executiontime
+    @converter
+    def check_all_and_any(self):
+        """return dictionary with keys(all, any)
+        check if all/any Transport objects max_speed > 100"""
+        return {'all': all(transport.max_speed > 100 for transport in self.transports),
+                'any': any(transport.max_speed > 100 for transport in self.transports)}
+
+    def add_transport(self, transport):
+        """add transport to list"""
+        self.transports.append(transport)
+
+    @executiontime
+    def find_all_with_id_greater_than(self, number):
+        """get list <Transport> when id > number"""
+        return [obj for obj in self.transports if obj.identifier > number]
+
+    @executiontime
+    def find_all_with_current_speed(self):
+        """get list <Transport> when current_speed > number"""
+        return [obj for obj in self.transports if obj.current_speed > 0]
+
+    @executiontime
+    def find_all_trolleybuses_with_passengers(self, number):
+        """get list <Trolleybus> when passengers == number"""
+        return [obj for obj in self.transports
+                if isinstance(obj, Trolleybus) if obj.passengers is number]
--- models/transport.py
+++ models/transport.py
@@ -4,12 +4,22 @@ from abc import ABC, abstractmethod
 
 class Transport(ABC):  # pylint: disable=too-few-public-methods
     """Abstract Transport"""
+    transport_marks = {}
 
     # pylint: disable=too-many-arguments
     def __init__(self, identifier=0, max_speed=0, current_speed=0):
         self.identifier = identifier
         self.max_speed = max_speed
         self.current_speed = current_speed
+
+    # pylint: disable=unidiomatic-typecheck
+    def get_dict_with_type(self, value_type):
+        """return dictionary of attributes"""
+        return {key: value for key, value in self.__dict__.items()
+                if type(value) is value_type}
+
+    def __iter__(self):
+        return iter(self.transport_marks)
 
     @abstractmethod
     def accelerate(self, speed):
